f5e56eb4e0cbd695c1226d8eff527928580cd916
Ilya Kolpakov <kolpakoph@gmail.com>
2022-07-05 13:51:27 +0300

    first commit

---
+++ FDataBase.py
@@ -0,0 +1,318 @@
+import sqlite3
+import time
+import math, os
+from datetime import datetime
+import shutil
+import random
+
+
+class FDataBase:
+    
+
+    def __init__(self, db):
+        self.__db = db
+        self.__cur = db.cursor()
+
+    def getPost(self, url):
+        try:
+            self.__cur.execute(f"SELECT title, content, date, visits FROM blog WHERE url LIKE '{url}' LIMIT 1")
+            res = self.__cur.fetchone()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Error gettin post from db "+str(e))
+ 
+        return (False, False, False)
+    
+    def getShawsData(self):
+        sql = '''SELECT rowid, * FROM shawarma ORDER BY rating'''
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res: return res
+        except:
+            print("Error reading DB")
+        return []
+
+    def getCatData(self):
+        sql = '''SELECT rowid, * FROM cat'''
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res: return res
+        except:
+            print("Error reading DB")
+        return []
+    
+    def getBooksData(self):
+        sql = '''SELECT rowid, * FROM books'''
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res: return res
+        except:
+            print("Error reading DB")
+        return []
+
+
+
+    def getBlogData(self, search, sort):
+        if search != '' and sort == 'Новые':
+            sql = f"SELECT rowid, * FROM blog WHERE title LIKE '%{search.lower()}%' ORDER BY rowid DESC"
+        elif search != '' and sort == 'Старые':
+            sql = f"SELECT rowid, * FROM blog WHERE title LIKE '%{search.lower()}%' ORDER BY rowid ASC"
+        if search != '' and sort == 'Популярные':
+            sql = f"SELECT rowid, * FROM blog WHERE title LIKE '%{search.lower()}%' ORDER BY visits DESC"
+        elif search != '' and sort == 'Непопулярные':
+            sql = f"SELECT rowid, * FROM blog WHERE title LIKE '%{search.lower()}%' ORDER BY visits ASC"
+
+        elif search == '' and sort == 'Новые':
+            sql = f"SELECT rowid, * FROM blog ORDER BY rowid DESC"
+        elif search == '' and sort == 'Старые':
+            sql = f"SELECT rowid, * FROM blog ORDER BY rowid ASC"
+        if search == '' and sort == 'Популярные':
+            sql = f"SELECT rowid, * FROM blog ORDER BY visits DESC"
+        elif search == '' and sort == 'Непопулярные':
+            sql = f"SELECT rowid, * FROM blog ORDER BY visits ASC"
+
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res: return res
+        except:
+            print("Error reading DB")
+        return []
+
+    def getMySitesData(self):
+        sql = '''SELECT rowid, * FROM links'''
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res: return res
+        except:
+            print("Error reading DB")
+        return []
+
+    def getComments(self, url):
+        sql = f"SELECT rowid, * FROM comments WHERE url LIKE '{url}'"
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res: return res
+        except:
+            print("Error reading DB")
+        return []
+
+    def getURL(self, id):
+        sql = f"SELECT url FROM comments WHERE rowid = {id}"
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res: return res[0]['url']
+        except:
+            print("Error reading DB")
+        return []
+    
+    def addPost(self, title, url, content):
+        try:
+            date = str(datetime.now() .strftime("%d.%m.%Y"))
+            visits = 0
+            self.__cur.execute(f"INSERT INTO blog VALUES('{url}', '{content}', {visits}, '{title.lower()}', '{date}')")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while adding "+str(e))
+            return False
+        return True
+
+
+    def addComment(self, comment, admin, reply, url):
+        adj = [
+            "милейший",
+            "зоркий",
+            "дьявольский",
+            "банановый",
+            "добрый",
+            "злой",
+            "соседский",
+            "ржавый",
+            "мокрый",
+            "кривой",
+            "старый",
+            "хипстерский",
+            "настоящий",
+            "летающий",
+            "тёмный"
+            ]
+        noun = [
+            'огородник', 
+            "сверчок", 
+            "дьявол", 
+            "пакет", 
+            "десерт", 
+            "тапок", 
+            "кот", 
+            "инженер", 
+            "полковник", 
+            "гвоздодёр", 
+            "косинус",
+            "моряк",
+            "куст",
+            "подстрекатель",
+            "плинтус"]
+        try:
+            if admin:
+                name = "САМЫЙ ГЛАВНЫЙ"
+                avatar = 0
+            else:
+                name = random.choice(adj).upper() + ' ' + random.choice(noun).upper()
+                avatar = random.randint(1, 9)
+            
+            self.__cur.execute(f"INSERT INTO comments VALUES('{name}', '{comment}', '{reply}', {avatar}, '{url}')")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while adding "+str(e))
+            return False
+
+    def addBook(self, author, title, file):
+        try:
+            self.__cur.execute(f"INSERT INTO books VALUES('{author}', '{title}', '{file}')")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while adding "+str(e))
+            return False
+        return True
+
+    
+
+
+    def addCatPhoto(self, description, file):
+        try:
+            tm = math.floor(time.time())
+            self.__cur.execute(f"INSERT INTO cat VALUES('{description}', '{file}')")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while adding "+str(e))
+            return False
+        return True
+
+    def addShawarma(self, name, place, mark, rating, link):
+        try:
+            self.__cur.execute(f"UPDATE shawarma SET rating = rating + 1 WHERE rating >= {rating}")
+            self.__db.commit()
+            self.__cur.execute(f"INSERT INTO shawarma VALUES({rating}, '{name}', '{place}', {mark}, '{link}')")
+            self.__db.commit()
+            print("I did")
+        except sqlite3.Error as e:
+            print("Error while adding "+str(e))
+            return False
+        return True
+
+    def addSite(self, description, link):
+        try:
+            self.__cur.execute(f"INSERT INTO links VALUES('{description}', 'http://{link}')")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while adding "+str(e))
+            return False
+        return True
+
+    def addVisit(self, url):
+        try:
+            self.__cur.execute(f"UPDATE blog SET visits = visits + 1 WHERE url LIKE '{url}'")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while updating db "+str(e))
+            return False
+        return True
+
+
+
+
+    def deletePost(self, id):
+        try:
+            self.__cur.execute(f"SELECT rowid, * FROM blog WHERE rowid = {id}")
+            self.__db.commit()
+            res = self.__cur.fetchall()[0]['url']
+            if os.path.exists('static/files/posts_res/' + res):
+                shutil.rmtree('static/files/posts_res/' + res)
+            self.__cur.execute(f"DELETE FROM comments WHERE url = '{res}'")
+            self.__cur.execute(f"DELETE FROM blog WHERE rowid = {id}")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while deleting "+str(e))
+            return False
+        return True
+
+    def deleteBook(self, id):
+        try:
+            self.__cur.execute(f"SELECT rowid, * FROM books WHERE rowid = {id}")
+            self.__db.commit()
+            res = self.__cur.fetchall()[0]['file']
+            if os.path.exists(res):
+                os.remove(res)
+            self.__cur.execute(f"DELETE FROM books WHERE rowid = {id}")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while deleting "+str(e))
+            return False
+        return True
+
+    def deleteCatPhoto(self, id):
+        try:
+            self.__cur.execute(f"SELECT rowid, * FROM cat WHERE rowid = {id}")
+            self.__db.commit()
+            res = self.__cur.fetchall()[0]['url']
+            if os.path.exists(res):
+                os.remove(res)
+            self.__cur.execute(f"DELETE FROM cat WHERE rowid = {id}")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while deleting "+str(e))
+            return False
+        return True
+
+    def deleteShawarma(self, id):
+        try:
+            self.__cur.execute(f"SELECT rowid, * FROM shawarma WHERE rowid = {id}")
+            self.__db.commit()
+            res = self.__cur.fetchall()[0]['rating']
+            self.__cur.execute(f"UPDATE shawarma SET rating = rating - 1 WHERE rating >= {res}")
+            self.__db.commit()
+            self.__cur.execute(f"DELETE FROM shawarma WHERE rowid = {id}")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while deleting "+str(e))
+            return False
+        return True
+
+    def deleteSite(self, id):
+        try:
+            self.__cur.execute(f"DELETE FROM links WHERE rowid = {id}")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while deleting "+str(e))
+            return False
+        return True
+
+    def deleteComment(self, id):
+        try:
+            self.__cur.execute(f"DELETE FROM comments WHERE rowid = {id}")
+            self.__cur.execute(f"DELETE FROM comments WHERE reply = {id}")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error while deleting "+str(e))
+            return False
+        return True
+
+    def getPassword(self):
+        try:
+            self.__cur.execute(f"SELECT * FROM admin")
+            res = self.__cur.fetchall()
+            if res: return res[0]['password']
+        except sqlite3.Error as e:
+            print("Error while deleting "+str(e))
+            return False
+        return True
+
+
+
---
+++ kolpa_site.py
@@ -0,0 +1,206 @@
+from crypt import methods
+from distutils.log import debug
+from attr import field
+from flask import Flask, render_template, url_for, request, session, redirect, g, flash
+import os, sqlite3, re
+from FDataBase import FDataBase
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+DATABASE = "kolpa.db"
+DEBUG = True
+SECRET_KEY = 'sfdggrege21dsvdfvdvdad3'
+PASSWORD = '123'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.static_folder,'kolpa.db')))
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрываем соединение с БД, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route("/shawarma_rating", methods=["POST", "GET"])
+def shawarma():
+    if request.method == "POST" and session['admin']:
+        if request.form['place'] != "" and request.form['link'] != "" and request.form['name'] != "" and request.form['mark'] != "" and request.form['rating'] != "":
+            dbase.addShawarma(request.form['name'], request.form['place'], request.form['mark'], request.form['rating'], request.form['link'])
+            flash('Added', category='success')
+        else:
+            flash('Error', category='error')
+        return redirect(url_for('shawarma'))
+    return render_template('shawarma_rating.html', shaws = dbase.getShawsData(), admin = session['admin'])  
+
+
+@app.route("/cat", methods=["POST", "GET"])
+def cat():
+    if request.method == 'POST' and session['admin']:
+        print(request.files)
+        if 'photo' not in request.files:
+            flash('Cant read a file', category='error')
+            return redirect(request.url)
+        file = request.files['photo']
+        if file.filename == '':
+            flash('There is no file', category='error')
+            return redirect(request.url)
+        if file:         
+            time = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+            file.save(os.path.join("static/files/kusya", str(time) + '.png'))
+            dbase.addCatPhoto(request.form['description'], 'static/files/kusya/' + str(time) + '.png')
+            flash('Added', category='success')
+        return redirect(url_for('cat'))
+    return render_template('kusya.html', cat = dbase.getCatData(), admin = session['admin'])  
+
+@app.route("/blog/<url>", methods=["POST", "GET"])
+def showPost(url):
+    dbase.addVisit(url)
+    if request.method == "POST":
+        if request.form['comment'] != '':           
+            dbase.addComment(request.form['comment'], int(session['admin']), 0, url)
+        return redirect("/blog/" + url)
+    title, post, date, visits = dbase.getPost(url)
+    comments = dbase.getComments(url)
+    return render_template('post.html',  visits = visits, date = date, comments = comments, title=title, post=post, admin = session['admin'], url=url)
+
+@app.route("/blog", methods=["POST", "GET"])
+def blog():
+    search = ''
+    sort = 'Новые'
+    if request.method  == 'POST' and session['admin'] and request.form.get('post') != None:
+        if request.form['title'] != "" and request.form['content'] != "" and request.form['url'] != "":
+            if os.path.isdir('static/files/posts_res/' + request.form['url']) :
+                flash('Error', category='error')
+            else:
+                os.mkdir('static/files/posts_res/' + request.form['url'])
+                files = request.files.getlist('images')
+                for file in files:
+                    if file.filename != '':
+                        file.save(os.path.join("static/files/posts_res/" + request.form['url'], file.filename))
+                base = "../static/files/posts_res/" + request.form['url']
+                content = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
+                          "\\g<tag>"+base+"/\\g<url>>",
+                          request.form['content'])
+                dbase.addPost(request.form['title'], request.form['url'], content)
+                flash('Added', category='success')
+        return redirect(url_for('blog'))
+    elif request.method  == 'POST' and request.form.get("search") != None:
+        sort = request.form["Sort method"]
+        search = request.form["text"]
+    return render_template('blog.html', posts=dbase.getBlogData(search, sort), admin = session['admin'])  
+
+@app.route("/resume")
+def resume():
+    return render_template('resume.html') 
+
+@app.route("/books", methods=["POST", "GET"])
+def books():
+    if request.method == 'POST' and session['admin']:
+        print(request.files)
+        if 'book' not in request.files:
+            flash('Cant read a file', category='error')
+            return redirect(request.url)
+        file = request.files['book']
+        if file.filename == '':
+            flash('There is no file', category='error')
+            return redirect(request.url)
+        if file:   
+            if request.form['title'] != "":
+                file.save(os.path.join("static/files/books", request.form['title']))
+                dbase.addBook(request.form['author'], request.form['title'], 'static/files/books/' + request.form['title'])
+                flash('Added', category='success')
+            else:
+                flash('No title', category='error')
+        return redirect(url_for('books'))
+    return render_template('books.html', books = dbase.getBooksData(), admin = session['admin'])
+
+
+@app.route("/admin", methods=["POST", "GET"])
+def admin():
+    if request.method == "POST":
+        if request.form.get('password') != None:    
+            hash = generate_password_hash(request.form['password'])   
+            if check_password_hash(hash, dbase.getPassword()):
+                session['admin'] = True
+        elif session['admin']:
+            session['admin'] = False
+        return redirect(url_for('admin'))
+    return render_template('admin.html',  admin = session['admin'])  
+
+
+@app.route("/mysites/<int:id>", methods=["POST", "GET"])
+def delete_site(id):
+    if request.method == "POST" and session['admin']:
+            dbase.deleteSite(id)
+    return redirect(url_for('mysites'))
+
+@app.route("/shawarma/<int:id>", methods=["POST", "GET"])
+def delete_shawarma(id):
+    if request.method == "POST" and session['admin']:
+            dbase.deleteShawarma(id)
+    return redirect(url_for('shawarma'))
+
+@app.route("/cat/<int:id>", methods=["POST", "GET"])
+def delete_catPhoto(id):
+    if request.method == "POST" and session['admin']:
+            dbase.deleteCatPhoto(id)
+    return redirect(url_for('cat'))
+
+
+@app.route("/blog/<int:id>", methods=["POST", "GET"])
+def delete_post(id):
+    if request.method == "POST" and session['admin']:
+            dbase.deletePost(id)
+    return redirect(url_for('blog'))
+
+@app.route("/blog/comment/<int:id>", methods=["POST", "GET"])
+def action_comment(id):
+    url = dbase.getURL(id)
+
+    if request.method == "POST" and session['admin'] and request.form.get('delete') != None:
+            dbase.deleteComment(id)
+    elif request.form['reply'] != '' and session['admin']:
+            dbase.addComment(request.form['reply'], 1, id, url)
+    return redirect('/blog/' + url)
+
+@app.route("/books/<int:id>", methods=["POST", "GET"])
+def delete_book(id):
+    if request.method == "POST" and session['admin']:
+            dbase.deleteBook(id)
+    return redirect(url_for('books'))
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('404.html'), 404
+
+@app.before_first_request
+def before_first_request():
+    if 'admin' not in session:
+            session['admin'] = False
+
+@app.before_request
+def before_request():
+   global dbase
+   db = get_db()
+   dbase = FDataBase(db)
+
+if __name__ == "__main__":
+    app.run(debug=True)
